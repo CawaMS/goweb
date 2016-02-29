a37c379faced4070d4e6428fbdd992574e4735ea
Catherine Wang <cawa@microsoft.com>
2016-02-28 19:22:18 -0800

    my 0th update

--- web.go
+++ web.go
@@ -14,7 +14,7 @@ func handler (w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "%s", err)
 	}
 	
-	fmt.Fprintf(w, "my first update")
+	fmt.Fprintf(w, "my 0th update")
 	fmt.Fprintf(w, "<h1>Web Client Host Name</h1> <div>%s</div>", string(host_name))
 	
 	res_mn, err := http.Get("http://restservice-demo-azure.marathon.mesos:5000/api/info/machinename")
